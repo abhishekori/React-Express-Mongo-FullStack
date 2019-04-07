5c94b87655012c3bbc61e1d29bdea85b2c9a0e88
abhishekori <abhikori1994@gmail.com>
2019-04-07 16:37:00 +0530

    list all the groups and corresponding lists

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-const path = require("path");
+// import path from 'path';
 
+const path = require("path");
+// export default {
 module.exports = {
-    mode : "development",
-    entry :path.resolve(__dirname,'src','app'),
-    output : {
-        path:path.resolve(__dirname,'dist'),
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src','app'),
+    output: {
+        path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js',
-        publicPath:'/'
+        publicPath: '/',
     },
-    resolve:{
-        extensions:['.js','jsx']
+    resolve: {
+        extensions: ['.js','.jsx']
     },
-    devServer :{
+    devServer: {
         historyApiFallback: true
     },
-    module:{
-        rules:[{
-            test:/\.jsx?/, 
-            loader: 'babel-loader'
-
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader:'babel-loader'
         }]
     }
 }
